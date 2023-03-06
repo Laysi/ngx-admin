e98813305eb3724c01a1af6be2c1eea51f1df211
LaysDragon <laysdra7265@gmail.com>
2023-03-06 15:42:28 +0800

    remove most of these demo and mock stuff

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,24 +1,22 @@
 import { NgModule } from '@angular/core';
-import { NbMenuModule } from '@nebular/theme';
+import { NbButtonModule, NbCardModule, NbMenuModule } from '@nebular/theme';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
-import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   imports: [
     PagesRoutingModule,
     ThemeModule,
     NbMenuModule,
-    DashboardModule,
-    ECommerceModule,
-    MiscellaneousModule,
+    NbCardModule,
+    NbButtonModule,
   ],
   declarations: [
     PagesComponent,
+    NotFoundComponent,
   ],
 })
 export class PagesModule {
